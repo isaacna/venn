7587ca9e730e333cd14a351dd1a9a7d2fedfa06c
isaac219 <isaac219@gmail.com>
2018-02-13 21:24:49 -0600

    ignore .idea

--- app/src/main/java/com/isaacna/projects/MainActivity.java
+++ app/src/main/java/com/isaacna/projects/MainActivity.java
@@ -1,13 +1,32 @@
 package com.isaacna.projects;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void viewProfile(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewCommunities(View view) {
+        Intent intent = new Intent(this, CommunitiesActivity.class);
+        startActivity(intent);
+    }
+
 }

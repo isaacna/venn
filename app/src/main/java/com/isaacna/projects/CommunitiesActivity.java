27530fc966a126b7e275c680e47430369ebf5373
isaac219 <isaac219@gmail.com>
2018-02-13 21:14:12 -0600

    gitignore

---
+++ app/src/main/java/com/isaacna/projects/CommunitiesActivity.java
@@ -0,0 +1,13 @@
+package com.isaacna.projects;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CommunitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_communities);
+    }
+}
